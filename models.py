ac68ebef1572a4d53e93ac055d68984586be3d91
Pradeep Pyatalo Jayarama <pradeeppj@gmail.com>
2020-10-26 17:14:27 -0500

    Create movie and actor models with migrations

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 import os
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, DateTime
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -13,15 +13,22 @@ def setup_db(app):
 
 def drop_db_and_create_new_db():
     db.drop_all()
-    db_create_all()
+    db.create_all()
 
-class Actors(db.Model):
+actor_movie = db.Table(
+    'actor_movie',
+    Column('actor_id', db.Integer, db.ForeignKey('actors.id'), primary_key=True),
+    Column('movie_id', db.Integer, db.ForeignKey('movies.id'), primary_key=True)
+)
+
+class Actor(db.Model):
     __tablename__ = 'actors'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(80), unique=True, nullable=False)
-    age = Column(Integer, nullable=False)
+    date_of_birth = Column(DateTime, nullable=False)
     gender = Column(String, nullable=False)
+    movies = db.relationship('Movie', secondary=actor_movie, backref=db.backref('movies', lazy=True))
 
     def format(self):
         return {
@@ -39,5 +46,34 @@ class Actors(db.Model):
         db.session.delete(self)
         db.session.commit()
 
+    @staticmethod
     def update(self):
         db.session.commit()
+
+class Movie(db.Model):
+    __tablename__ = 'movies'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(120), unique=True, nullable=False)
+    release_date = Column(DateTime, nullable=False)
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    @staticmethod
+    def update(self):
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'release_date': self.release_date
+        }
+
+    
