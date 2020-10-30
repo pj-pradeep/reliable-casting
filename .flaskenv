ab3040158db18765fd9af9aba38c800b4e37d051
Pradeep Pyatalo Jayarama <pradeeppj@gmail.com>
2020-10-29 22:53:09 -0500

    Refactor application settings to bash script from environment file

--- .flaskenv
+++
@@ -1,3 +0,0 @@
-export FLASK_APP=app/app.py
-export FLASK_ENV=development
-export FLASK_DEBUG=1
--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
 export DATABASE_URL='postgres://postgres@localhost:5432/reliable-casting'
 export SQLALCHEMY_TRACK_MODIFICATIONS=False
-export APP_SECRET_KEY=b'Y\xban\x86e\x90\x13\xe2qQ#\xd2\xd4\x10\xdd}'
+export APP_SECRET_KEY='Cang35avLLQ$Df'
 export AUTH0_CALLBACK_URL='http://localhost:5000/callback'
 export AUTH0_CLIENT_ID='rv3llI8BPjRILs8KcB58EWaokA4d6NHx'
 export AUTH0_CLIENT_SECRET='F_u59lANTwIOP386dWVDeiPEYvfGeTL4gqfTenP17fLx7kV1R6p7EVxqVXAsXIbD'
 export AUTH0_DOMAIN='dev-wr0-fzj9.us.auth0.com'
 export AUTH0_AUDIENCE='https://api.reliable-casting.com'
+export FLASK_APP=app/app.py
+export FLASK_ENV=development
+export FLASK_DEBUG=1
